eb4bef640faf20de9e67ea5335a4603ea4ac19b2
ViswajithJ <viswajith.j.official@gmail.com>
2022-12-09 23:55:46 +0530

    Stack implemented using Linked list

---
+++ Linked-List/stack_using_linkedlist.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Define the structure for node
+struct node {
+	int data;
+	struct node *link;
+};
+
+//top is the pointer to first node
+struct node *top = NULL;
+
+void display() {
+	struct node *ptr;
+	printf("Elements in the stack:-\n\n");
+	ptr = top;
+	if (ptr == NULL) {
+		printf("Stack Underflow.\n\n");      //top is NULL => no nodes
+	} else {
+		while (ptr->link != NULL) {
+			printf("%d\t", ptr->data);
+			ptr = ptr->link;
+		}
+		printf("%d\n", ptr->data);
+	}
+	printf("\n");
+}
+
+
+void push() {
+	struct node *temp = (struct node *) malloc(sizeof(struct node));
+	printf("Enter the element to be pushed to the stack: ");
+	scanf("%d", &temp->data);
+	temp->link = top; 
+	top = temp;	  
+	display();
+}
+
+
+void pop() {
+	struct node *ptr;
+	ptr = top;
+	if(ptr == NULL) {
+		printf("Stack Underflow\n\n");
+		return;
+	} else {
+		int data = ptr->data;
+		printf("Deleted element : %d\n", data);
+		top = ptr->link; 
+		ptr->link = NULL; 
+		free(ptr);
+	}
+	display();
+}
+
+int main() {
+	int optionNum;
+
+	printf("Stack Implementation using Linked List.\n\n");
+	do {
+	 	printf("Available operations:- \n");
+		printf("\n1. Push an element into the stack.\n");
+		printf("2. Pop an element from the stack.\n");
+		printf("3. Display the elements of the stack.\n");
+		printf("4. EXIT\n\n");
+		printf("Enter the option number: ");
+		scanf("%d", &optionNum);
+
+		if (optionNum == 1) {
+			push();
+		} else if (optionNum == 2) {
+			pop();
+		} else if (optionNum == 3) {
+			display();
+		} else if (optionNum == 4) {
+			break;
+		} else {
+			printf("Invalid option number.\n\n");
+		}
+	}while(1);
+}
+
